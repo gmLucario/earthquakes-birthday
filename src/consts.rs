e4118cf6ea6f1d25de128a823aff1eef7f0661d4
gmlukario <gmlukario@gmail.com>
2022-10-03 17:09:25 -0500

    🎨: cargo clippy

--- src/consts.rs
+++ src/consts.rs
@@ -1,4 +1,4 @@
-pub const FORMAT_STR_DATETIME_INPUT: &'static str = "%Y-%m-%d &nbsp; %H:%M:%S";
-pub const FORMAT_STR_DATETIME_OUTPUT: &'static str = "%Y-%m-%dT%H:%M:%S";
+pub const FORMAT_STR_DATETIME_INPUT: &str = "%Y-%m-%d &nbsp; %H:%M:%S";
+pub const FORMAT_STR_DATETIME_OUTPUT: &str = "%Y-%m-%dT%H:%M:%S";
 pub const MIN_MAGNITUDE_EARTHQUAKE: f32 = 5.0;
 pub const MIN_AMOUNT_EARTHQUAKE: usize = 7;
